52b274365f15b4363e9616b6d4175b99959cd47e
Karlo Smid <karlo.smid@gmail.com>
2022-09-16 18:46:29 +0200

    feat: 🎸 hints for duplicate colors

--- lib/mastermind_web/live/game.ex
+++ lib/mastermind_web/live/game.ex
@@ -252,16 +252,43 @@ defmodule MastermindWeb.Game do
     |> IO.inspect()
   end
 
-  defp set_hints(assigns) do
+  def set_hints(assigns) do
     pins = assigns.pins
     code = assigns.code
 
-    Enum.reduce(0..3, [], fn index, acc ->
-      cond do
-        Enum.at(pins, index) == Enum.at(code, index) -> [0 | acc]
-        Enum.member?(code, Enum.at(pins, index)) -> [1 | acc]
-        true -> [2 | acc]
-      end
-    end)
+    pins_codes =
+      Enum.reduce(0..3, %{pins: [], code: []}, fn index, acc ->
+        if Enum.at(pins, index) ==
+             Enum.at(code, index) do
+          Map.put(acc, :pins, Map.get(acc, :pins) ++ [0])
+          |> Map.put(:code, Map.get(acc, :code) ++ [0])
+        else
+          Map.put(acc, :pins, Map.get(acc, :pins) ++ [Enum.at(pins, index)])
+          |> Map.put(:code, Map.get(acc, :code) ++ [Enum.at(code, index)])
+        end
+      end)
+
+    pins_freq = Enum.frequencies(Enum.filter(pins_codes.pins, fn x -> x != 0 end))
+    code_freq = Enum.frequencies(Enum.filter(pins_codes.code, fn x -> x != 0 end))
+
+    colors =
+      pins_codes.pins
+      |> Enum.filter(fn pin -> pin != 0 end)
+      |> Enum.uniq()
+      |> Enum.reduce([], fn pin, acc ->
+        cond do
+          Map.get(code_freq, pin) == nil ->
+            acc
+
+          Map.get(pins_freq, pin) > Map.get(code_freq, pin) ->
+            acc ++
+              Enum.map(1..(Map.get(pins_freq, pin) - Map.get(code_freq, pin)), fn _x -> 1 end)
+
+          Map.get(pins_freq, pin) <= Map.get(code_freq, pin) ->
+            acc ++ Enum.map(1..Map.get(pins_freq, pin), fn _x -> 1 end)
+        end
+      end)
+
+    Enum.filter(pins_codes.pins, fn pin -> pin == 0 end) ++ colors
   end
 end
