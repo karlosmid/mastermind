c9df9d3b8b0e953801f5a9626fae1ba223f033a4
Matija Krajnik <matija.krajnik90@gmail.com>
2025-06-13 15:58:13 +0200

    🎨 refactor(ui): simplify game components and improve user interaction
    - Replace multiple hardcoded toggle pin event handlers with unified color selection system
    - Convert string-based color system to atom-based approach for better type safety
    - Refactor game components to use Phoenix LiveView icons instead of inline SVG
    - Implement selected color state management for improved UX
    - Simplify code generation logic with Enum.take_random instead of multiple concatenations
    - Update pin component with dynamic color classes and interactive states
    - Reorganize game layout with better visual hierarchy and responsive design
    - Refactor tests to use atom-based color system consistently
    - Add hover effects and visual feedback for better user interaction

--- lib/mastermind_web/live/game_live/game_live.ex
+++ lib/mastermind_web/live/game_live/game_live.ex
@@ -9,128 +9,62 @@ defmodule MastermindWeb.GameLive do
     socket =
       socket
       |> assign(:no_steps, 12)
-      |> assign(:pins, ["empty", "empty", "empty", "empty"])
+      |> assign(:colors, Utils.colors())
+      |> assign(:selected_color, nil)
+      |> assign_empty_pins()
       |> assign(:tries, [])
       |> assign(:code, Utils.set_code())
 
     {:ok, socket}
   end
 
-  def handle_event("play", _value, socket) do
-    socket =
-      assign(
-        socket,
-        :tries,
-        [
-          %{hints: Utils.set_hints(socket.assigns), pins: socket.assigns.pins}
-        ] ++ socket.assigns.tries
-      )
+  defp assign_empty_pins(socket) do
+    pins = Enum.map(1..4, fn _ -> :empty end)
+    assign(socket, :pins, pins)
+  end
 
-    socket = assign(socket, :pins, ["empty", "empty", "empty", "empty"])
+  def handle_event("play", _value, %{assigns: %{pins: pins, tries: tries}} = socket) do
+    tries = [%{hints: Utils.set_hints(socket.assigns), pins: pins} | tries]
+
+    socket =
+      socket
+      |> assign(:tries, tries)
+      |> assign_empty_pins()
+
     {:noreply, socket}
   end
 
   def handle_event("restart", _value, socket) do
-    socket = assign(socket, :code, Utils.set_code())
-    socket = assign(socket, :tries, [])
-    socket = assign(socket, :pins, ["empty", "empty", "empty", "empty"])
+    socket =
+      socket
+      |> assign(:code, Utils.set_code())
+      |> assign(:tries, [])
+      |> assign(:selected_color, nil)
+      |> assign_empty_pins()
+
     {:noreply, socket}
   end
 
-  def handle_event("toggle-1-red", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(0, "fill-red-500", socket.assigns.pins))}
+  def handle_event(
+        "select_color",
+        %{"color" => color},
+        %{assigns: %{selected_color: selected_color}} = socket
+      ) do
+    color = String.to_existing_atom(color)
+    selected_color = if color == selected_color, do: nil, else: color
+    socket = assign(socket, :selected_color, selected_color)
+    {:noreply, socket}
   end
 
-  def handle_event("toggle-2-red", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(1, "fill-red-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-3-red", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(2, "fill-red-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-4-red", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(4, "fill-red-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-1-blue", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(0, "fill-blue-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-2-blue", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(1, "fill-blue-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-3-blue", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(2, "fill-blue-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-4-blue", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(4, "fill-blue-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-1-yellow", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(0, "fill-yellow-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-2-yellow", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(1, "fill-yellow-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-3-yellow", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(2, "fill-yellow-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-4-yellow", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(4, "fill-yellow-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-1-orange", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(0, "fill-orange-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-2-orange", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(1, "fill-orange-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-3-orange", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(2, "fill-orange-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-4-orange", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(4, "fill-orange-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-1-green", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(0, "fill-green-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-2-green", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(1, "fill-green-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-3-green", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(2, "fill-green-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-4-green", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(4, "fill-green-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-1-fuchsia", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(0, "fill-fuchsia-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-2-fuchsia", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(1, "fill-fuchsia-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-3-fuchsia", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(2, "fill-fuchsia-500", socket.assigns.pins))}
-  end
-
-  def handle_event("toggle-4-fuchsia", _value, socket) do
-    {:noreply, assign(socket, :pins, Utils.set_pin(4, "fill-fuchsia-500", socket.assigns.pins))}
+  def handle_event(
+        "toggle_pin",
+        %{"index" => index},
+        %{assigns: %{pins: pins, selected_color: selected_color}} = socket
+      ) do
+    index = String.to_integer(index)
+    pins = Utils.set_pin(index, selected_color, pins)
+    socket = assign(socket, :pins, pins)
+    {:noreply, socket}
   end
 
   defp stop(tries, no_steps) do
